081e030b923a31b94a18a134e19ae8bd3873764e
Digital Site <113808139+sitetag@users.noreply.github.com>
2022-09-27 23:57:28 -0300

    1

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    // menu
+    $('#nav-toggle').click(function(e){
+        e.preventDefault();
+        $(this).toggleClass('active');
+        $('.header-collapse').toggleClass('active');
+    });
+    // carousel principal
+    $('.owl-carousel').owlCarousel({
+        items:
+    })
+});
